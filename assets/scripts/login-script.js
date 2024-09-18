4000d430a757137d1ff82b990db73020c1ff8d73
Yusufamey <a171b4ky2439@bangkit.academy>
2024-09-18 20:52:18 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,58 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/*@module Login-Script */
+/**
+*Membuat variabel Login loginFormElement untuk tampilan form.
+*@constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+*Membuat variabel Login loginFormElement untuk tampilan email.
+*@constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+*Membuat variabel Login loginFormElement untuk tampilan form.
+*@constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+*Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+* @constant{string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+* Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+*/
 const expectedPassword = 'superpassword';
 
+/* Comment : Menambahakn aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  *Mebuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+  *constant {string}
+  */
   const email = inputEmailElement.value;
+  
+  /**
+*Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
+*constant {string}
+*/
   const password = inputPasswordElement.value;
+
+
+  /* comment: Memastikan bahwa nilai email dan passsword sesuai dengan nilai yang tersipan.  */
   if (email == expectedEmail && password == expectedPassword) {
+
+    /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
+
+    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
